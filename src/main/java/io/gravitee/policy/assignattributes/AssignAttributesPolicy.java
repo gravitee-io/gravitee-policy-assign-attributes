f81f6e06ca19f529ef1e6ce80f12526eff195a69
Florent CHAMFROY <florent.chamfroy@graviteesource.com>
2020-10-15 16:09:08 +0200

    feat(processor): add path parameters processing
    * Use `EvaluableRequest` and `EvaluableResponse` from gateway-api
    * Update gravitee dependancies version in pom
    
    Closes gravitee-io/issues#4431

--- src/main/java/io/gravitee/policy/assignattributes/AssignAttributesPolicy.java
+++ src/main/java/io/gravitee/policy/assignattributes/AssignAttributesPolicy.java
@@ -19,6 +19,8 @@ import io.gravitee.gateway.api.ExecutionContext;
 import io.gravitee.gateway.api.Request;
 import io.gravitee.gateway.api.Response;
 import io.gravitee.gateway.api.buffer.Buffer;
+import io.gravitee.gateway.api.el.EvaluableRequest;
+import io.gravitee.gateway.api.el.EvaluableResponse;
 import io.gravitee.gateway.api.stream.BufferedReadWriteStream;
 import io.gravitee.gateway.api.stream.ReadWriteStream;
 import io.gravitee.gateway.api.stream.SimpleReadWriteStream;
