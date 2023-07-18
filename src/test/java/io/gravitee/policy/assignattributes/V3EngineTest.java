bd51e10421397a468347c82acf2e99e66e6c9102
Julien Giovaresco <dev@giovaresco.fr>
2023-07-18 10:17:45 +0200

    chore(deps): update gravitee-parent
    BREAKING CHANGE: require Java17

--- src/test/java/io/gravitee/policy/assignattributes/V3EngineTest.java
+++ src/test/java/io/gravitee/policy/assignattributes/V3EngineTest.java
@@ -1,11 +1,11 @@
-/**
- * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+/*
+ * Copyright © 2015 The Gravitee team (http://gravitee.io)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *         http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,9 +17,7 @@ package io.gravitee.policy.assignattributes;
 
 import io.gravitee.apim.gateway.tests.sdk.AbstractPolicyTest;
 import io.gravitee.apim.gateway.tests.sdk.annotations.GatewayTest;
-import io.gravitee.apim.gateway.tests.sdk.configuration.GatewayConfigurationBuilder;
 import io.gravitee.apim.gateway.tests.sdk.policy.PolicyBuilder;
-import io.gravitee.definition.model.Api;
 import io.gravitee.definition.model.ExecutionMode;
 import io.gravitee.plugin.policy.PolicyPlugin;
 import io.gravitee.policy.assignattributes.configuration.AssignAttributesPolicyConfiguration;
